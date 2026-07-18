cd6e55bdea4252264f494d4fd9983523eb9d8fcb
Arpita Sharma <arsharma0429@gmail.com>
2026-07-18 16:15:53 +0530

    Added deployment config and requirements.txt

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,10 @@
-MYSQL_HOST = "localhost"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "@Arpita110666"
-MYSQL_DB = "resqmeal"
+import os
+from dotenv import load_dotenv
+
+# Load local .env file if it exists
+load_dotenv()
+
+MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
+MYSQL_USER = os.environ.get("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "@Arpita110666")
+MYSQL_DB = os.environ.get("MYSQL_DB", "resqmeal")
